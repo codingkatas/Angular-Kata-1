c8179c35231fcf93d966c7f7aa886ef33f5c5036
Soeren Pedersen <speconsult@gmail.com>
2013-08-25 21:58:23 +0200

    initial

---
+++ src/main/webapp/app/kata1/app.js
@@ -0,0 +1,13 @@
+/*jslint browser : true, continue : true,
+ devel : true, indent : 2, maxerr : 50,
+ newcap : true, nomen : true, plusplus : true,
+ regexp : true, sloppy : true, vars : false,
+ white : false
+ */
+/*global window, angular, kata1 */
+
+namespace('kata1');
+
+var kata1Module = angular.module('Kata1', []);
+
+
