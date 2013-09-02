16781acad971da441223a902b4eb3f4a966852d8
soeren.pedersen <svx@topdanmark.dk>
2013-09-02 09:57:36 +0200

    fixed small setup-bugs

--- karma.conf.js
+++ karma.conf.js
@@ -1,80 +1,78 @@
 // Karma configuration
-// Generated on Mon Aug 26 2013 20:17:56 GMT+0200 (Rom, sommertid)
-
-module.exports = function (config) {
-    config.set({
-
-        // base path, that will be used to resolve files and exclude
-        basePath:'',
+// Generated on Fri Jun 21 2013 07:29:05 GMT+0200 (Vesteuropa, sommertid)
 
 
-        // frameworks to use
-        frameworks:['jasmine'],
+// base path, that will be used to resolve files and exclude
+basePath = '';
 
 
-        // list of files / patterns to load in the browser
-        files:[
-            'src/main/webapp/lib/sugar-1.3.9.min.js',
-            'src/main/webapp/lib/jquery-2.0.3.min.js',
-            'src/main/webapp/lib/angular-1.2.0/angular.min.js',
-            'src/main/webapp/lib/angular-1.2.0/angular-mocks.js',
-            'src/main/webapp/lib/angular-1.2.0/angular-resource.min.js',
-            'src/main/webapp/lib/angular-1.2.0/angular-route.min.js',
-            'src/main/webapp/lib/ui-bootstrap-tpls-0.5.0.min.js',
-            'src/main/webapp/app/kata1/util.js',
-            'src/main/webapp/app/kata1/app.js',
-            'src/main/webapp/app/kata1/**/*.js',
-            'src/test/webapp/app/**/*.js'
+// list of files / patterns to load in the browser
+files = [
+    JASMINE,
+    JASMINE_ADAPTER,
 
-        ],
+    'src/main/webapp/lib/sugar-1.3.9.min.js',
+    'src/main/webapp/lib/jquery-2.0.3.min.js',
+    'src/main/webapp/lib/angular-1.2.0/angular.min.js',
+    'src/main/webapp/lib/angular-1.2.0/angular-mocks.js',
+    'src/main/webapp/lib/angular-1.2.0/angular-resource.min.js',
+    'src/main/webapp/lib/angular-1.2.0/angular-route.min.js',
+    'src/main/webapp/lib/ui-bootstrap-tpls-0.5.0.min.js',
+    'src/main/webapp/app/kata/util.js',
+    'src/main/webapp/app/kata/app.js',
+    'src/main/webapp/app/kata/**/*.js',
+    'src/test/webapp/app/**/*.js'
 
-
-        // list of files to exclude
-        exclude:[
-
-        ],
+];
 
 
-        // test results reporter to use
-        // possible values: 'dots', 'progress', 'junit', 'growl', 'coverage'
-        reporters:['progress'],
+// list of files to exclude
+exclude = [
+
+];
 
 
-        // web server port
-        port:9876,
+// test results reporter to use
+// possible values: 'dots', 'progress', 'junit'
+reporters = ['progress'];
 
 
-        // enable / disable colors in the output (reporters and logs)
-        colors:true,
+// web server port
+port = 9876;
 
 
-        // level of logging
-        // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
-        logLevel:config.LOG_INFO,
+// cli runner port
+runnerPort = 9100;
 
 
-        // enable / disable watching file and executing tests whenever any file changes
-        autoWatch:true,
+// enable / disable colors in the output (reporters and logs)
+colors = true;
 
 
-        // Start these browsers, currently available:
-        // - Chrome
-        // - ChromeCanary
-        // - Firefox
-        // - Opera
-        // - Safari (only Mac)
-        // - PhantomJS
-        // - IE (only Windows)
-        browsers:['Chrome'],
+// level of logging
+// possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
+logLevel = LOG_INFO;
 
 
-        // If browser does not capture in given timeout [ms], kill it
-        captureTimeout:60000,
+// enable / disable watching file and executing tests whenever any file changes
+autoWatch = true;
 
 
-        // Continuous Integration mode
-        // if true, it capture browsers, run tests and exit
-        singleRun:false
-    });
-};
+// Start these browsers, currently available:
+// - Chrome
+// - ChromeCanary
+// - Firefox
+// - Opera
+// - Safari (only Mac)
+// - PhantomJS
+// - IE (only Windows)
+browsers = ['Chrome'];
 
+
+// If browser does not capture in given timeout [ms], kill it
+captureTimeout = 60000;
+
+
+// Continuous Integration mode
+// if true, it capture browsers, run tests and exit
+singleRun = false;
